7adcd5e5b8bb4ff5bdd6eabd7c7fd9d8a3d987c4
Anju Kunjumon <akunjumon@sierrawireless.com>
2019-10-29 16:12:04 -0700

    [pa_wifi] Use TI driver kernel modules from Legato
    Legato hosts the TI Wireless kernel modules to avoid crowding the
    Rootfs in cases where WiFi extension card is not being used. This
    patch is to modify the TI Wireless platform adaptor to use the
    kernel modules hosted by legato.
    
    - tiwifi start and stop functionalities from
      /etc/init.d/tiwifi is brought in.
    - TI Wi-Fi kernel modules are now loaded Legato way (using kmod)
      not Linux way (insmod) as the modules now reside in Legato.
    - GPIO access is made through sysfs, where supported.
    
    Resolves: LE-13652
    Change-Id: I5cc56165f487c37e80aa84808ad7341d89c0ceb2
    Signed-off-by: Anju Kunjumon <akunjumon@sierrawireless.com>

--- service/platformAdaptor/ti/pa_wifi.sh
+++ service/platformAdaptor/ti/pa_wifi.sh
@@ -15,7 +15,7 @@ CMD=$1
 # WiFi interface
 IFACE=wlan0
 # If WLAN interface exists but can not be brought up, means WiFi hardware is inserted,
-# drivers are loaded successfully, but firmware failed to boot, tiwifi.sh returns 100
+# drivers are loaded successfully, but firmware failed to boot
 FIRMWAREFAILURE=100
 # If WLAN interface does not exist but driver is installed, means WiFi hardware is absent
 HARDWAREABSENCE=50
@@ -24,6 +24,275 @@ NODRIVER=100
 TIMEOUT=8
 # PATH
 export PATH=/legato/systems/current/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+
+#### TI specific functions ####
+# TI wireless wl18xx specific applications start or stop here
+# TI WIFI IoT board is managed by SDIO/MMC bus. Some signals need to be set
+# and managed before the SDIO/MMC module is inserted.
+# TI WIFI IoT conflicts with others devices using the SDIO/MMC bus
+
+# Interface does not exist
+TI_WIFI_PA_NO_IF_ERR=50
+
+# Interface could not be brought up
+TI_WIFI_PA_NO_IF_UP_ERR=100
+
+# Hardware settings function place holder; default to mangOH
+BOARD_CONFIG_WLAN=mangOH_cfg_wlan
+
+# Sysfs support for GPIO expander
+GPIOEXP_SYSFS=
+
+GPIO_EXPORT=/sys/class/gpio/v2/alias_export
+GPIO_UNEXPORT=/sys/class/gpio/v2/alias_unexport
+GPIO_DIR=/sys/class/gpio/v2/aliases_exported/
+if [ ! -e ${GPIO_EXPORT} ]; then
+    GPIO_EXPORT=/sys/class/gpio/export
+    GPIO_UNEXPORT=/sys/class/gpio/unexport
+    GPIO_DIR=/sys/class/gpio/gpio
+fi
+
+#For backward compatibility with LXSWI which does not support sysfs control of gpio expander
+gpioexp_set()
+{
+    # Enable all GPIOs on all EXPANDERs
+    gpioexp 1 1 enable >/dev/null || return 127
+
+    ### mangOH green has 3 expanders
+    # Set IOTO_RESET, GPIO#4/EXPANDER#3 - IOT0 Reset signal is disabled
+    gpioexp 3 4 output normal high >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "mangOH red board"
+
+        # Set IOT0_GPIO2 = 1 (WP GPIO13)
+        [ -d ${GPIO_DIR}13 ] || echo 13 >${GPIO_EXPORT}
+        echo out >${GPIO_DIR}13/direction
+        echo 1 >${GPIO_DIR}13/value
+
+        # Set IOT0_RESET = 1 (WP GPIO2)
+        [ -d ${GPIO_DIR}2 ] || echo 2 >${GPIO_EXPORT}
+        echo out >${GPIO_DIR}2/direction
+        echo 1 >${GPIO_DIR}2/value
+
+        # Clear SDIO_SEL, GPIO#9/EXPANDER#1 - Select the SDIO
+        gpioexp 1 9 output normal low >/dev/null || return 127
+    else
+        echo "mangOH green board"
+
+        # Set IOT0_GPIO2 = 1 (WP GPIO33)
+        [ -d ${GPIO_DIR}33 ] || echo 33 >${GPIO_EXPORT}
+        echo out >${GPIO_DIR}33/direction
+        echo 1 >${GPIO_DIR}33/value
+
+        # Clear SDIO_SEL, GPIO#13/EXPANDER#1 - Select the SDIO
+        gpioexp 1 13 output normal low >/dev/null || return 127
+    fi
+}
+
+#For backward compatibility with LXSWI which does not support sysfs control of gpio expander
+gpioexp_unset()
+{
+        # Clear IOTO_RESET, GPIO#4/EXPANDER#3 - IOT0 Reset signal is enabled
+        gpioexp 3 4 output normal low >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "mangOH red board"
+            # Set IOT0_RESET = 1 (WP GPIO2)
+            echo 0 >${GPIO_DIR}2/value
+
+            # Clear SDIO_SEL, GPIO#9/EXPANDER#1 - Deselect the SDIO
+            gpioexp 1 9 output normal high >/dev/null || return 127
+
+            # Reset IOT0_GPIO2 = 0 (WP GPIO13)
+            echo 0 >${GPIO_DIR}13/value
+
+            echo 13 >${GPIO_UNEXPORT}
+            echo 2 >${GPIO_UNEXPORT}
+        else
+            echo "mangOH green board"
+            # Set SDIO_SEL, GPIO#13/EXPANDER#1 - Deselect the SDIO
+            gpioexp 1 13 output normal high >/dev/null || return 127
+
+            # Reset IOT0_GPIO2 = 0 (WP GPIO33)
+            echo 0 >${GPIO_DIR}33/value
+
+            # Unexport the GPIO
+            echo 33 >${GPIO_UNEXPORT}
+       fi
+}
+
+# Check if sysfs support exists for gpio expander
+is_sysfs_gpioexp()
+{
+    if [ -z "$GPIOEXP_SYSFS" ]; then
+        #check for the support; is SDIO_SEL pin of expander present in sysfs?
+        grep SDIO_SEL /sys/class/gpio/v2/alias_map >/dev/null 2>&1
+    fi
+    [ $? -eq 0 ] && GPIOEXP_SYSFS=0 || GPIOEXP_SYSFS=1
+
+    return $GPIOEXP_SYSFS #0 for valid; 1 otherwise.
+}
+
+mangOH_cfg_wlan()
+{
+    case $1 in
+        SET_UP)
+            echo "Setting up GPIOs for wlan"
+            # GPIO expander settings
+            if is_sysfs_gpioexp ; then
+                ### mangOH green has 3 expanders
+                # Set IOTO_RESET, GPIO#4/EXPANDER#3 - IOT0 Reset signal is disabled
+                grep GPIO_IOT0_RESET /sys/class/gpio/v2/alias_map >/dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                    echo "mangOH green board"
+
+                    [ -d ${GPIO_DIR}GPIO_IOT0_RESET ] || echo GPIO_IOT0_RESET >${GPIO_EXPORT}
+                    echo out >${GPIO_DIR}GPIO_IOT0_RESET/direction
+                    echo 1 >${GPIO_DIR}GPIO_IOT0_RESET/value
+
+                    # Set IOT0_GPIO2 = 1 (WP GPIO33)
+                    [ -d ${GPIO_DIR}33 ] || echo 33 >${GPIO_EXPORT}
+                    echo out >${GPIO_DIR}33/direction
+                    echo 1 >${GPIO_DIR}33/value
+
+                    # Clear SDIO_SEL, GPIO#13/EXPANDER#1 - Select the SDIO
+                    [ -d ${GPIO_DIR}SDIO_SEL ] || echo SDIO_SEL >${GPIO_EXPORT}
+                    echo out >${GPIO_DIR}SDIO_SEL/direction
+                    echo 0 >${GPIO_DIR}SDIO_SEL/value
+                else
+                    echo "mangOH red board"
+
+                    # Set IOT0_GPIO2 = 1 (WP GPIO13)
+                    [ -d ${GPIO_DIR}13 ] || echo 13 >${GPIO_EXPORT}
+                    echo out >${GPIO_DIR}13/direction
+                    echo 1 >${GPIO_DIR}13/value
+
+                    # Set IOT0_RESET = 1 (WP GPIO2)
+                    [ -d ${GPIO_DIR}2 ] || echo 2 >${GPIO_EXPORT}
+                    echo out >${GPIO_DIR}2/direction
+                    echo 1 >${GPIO_DIR}2/value
+
+                    # Clear SDIO_SEL, GPIO#9/EXPANDER#1 - Select the SDIO
+                    [ -d ${GPIO_DIR}SDIO_SEL ] || echo SDIO_SEL >${GPIO_EXPORT}
+                    echo out >${GPIO_DIR}SDIO_SEL/direction
+                    echo 0 >${GPIO_DIR}SDIO_SEL/value
+                fi
+            else
+                gpioexp_set
+            fi # is_sysfs_gpioexp
+          ;;
+        REVERT)
+            echo "Clearing up GPIOs setting made for wlan"
+            # Revert GPIO settings in expanders
+            if is_sysfs_gpioexp ; then
+                # Clear IOTO_RESET, GPIO#4/EXPANDER#3 - IOT0 Reset signal is enabled
+                grep GPIO_IOT0_RESET /sys/class/gpio/v2/alias_map >/dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                   echo "mangOH green board"
+
+                    echo 0 >${GPIO_DIR}GPIO_IOT0_RESET/value
+                    # Set SDIO_SEL, GPIO#13/EXPANDER#1 - Deselect the SDIO
+                    echo 1 >${GPIO_DIR}SDIO_SEL/value
+
+                    # Reset IOT0_GPIO2 = 0 (WP GPIO33)
+                    echo 0 >${GPIO_DIR}33/value
+
+                    # Unexport the GPIO
+                    echo 33 >${GPIO_UNEXPORT}
+                    echo SDIO_SEL >${GPIO_UNEXPORT}
+                    echo GPIO_IOT0_RESET >${GPIO_UNEXPORT}
+                else
+                    echo "mangOH red board"
+                    # Reset IOT0_RESET = 0 (WP GPIO2)
+                    echo 0 >${GPIO_DIR}2/value
+
+                    # Set SDIO_SEL, GPIO#9/EXPANDER#1 - Deselect the SDIO
+                    echo 1 >${GPIO_DIR}SDIO_SEL/value
+
+                    # Reset IOT0_GPIO2 = 0 (WP GPIO13)
+                    echo 0 >${GPIO_DIR}13/value
+
+                    echo 13 >${GPIO_UNEXPORT}
+                    echo 2 >${GPIO_UNEXPORT}
+                    echo SDIO_SEL >${GPIO_UNEXPORT}
+               fi
+            else
+                gpioexp_unset
+            fi # is_sysfs_gpioexp
+          ;;
+    esac
+}
+
+ti_wifi_start()
+{
+    echo "Starting TI Wifi module"
+
+    # Add mdev rule for crda
+    grep crda /etc/mdev.conf > /dev/null
+    if [ $? -ne 0 ]; then
+        (mount | grep -q " on /etc type ") || \
+            (cp /etc/mdev.conf /tmp; mount -o bind /tmp/mdev.conf /etc/mdev.conf)
+        echo "\$COUNTRY=.. root:root 0660 */sbin/crda" >> /etc/mdev.conf
+    fi
+
+    lsmod | grep wlcore >/dev/null
+    if [ $? -ne 0 ]; then
+        #Do the necessary hardware settings for wlan
+        [ -z "$BOARD_CONFIG_WLAN" ] || ${BOARD_CONFIG_WLAN} SET_UP
+
+        #Load kernel modules
+        kmod load wlcore.ko || return 127
+        kmod load wlcore_sdio.ko || return 127
+        kmod load wl18xx.ko || return 127
+    fi
+    attempt=6
+    for i in $(seq 1 ${attempt})
+    do
+        if [ $i -ne 1 ]; then
+            sleep 1
+        fi
+        (ifconfig -a | grep wlan0 > /dev/null) && break
+    done
+    if [ $? -ne 0 ]; then
+        echo "Failed to start TI wifi, interface does not exist."
+        return ${TI_WIFI_PA_NO_IF_ERR}
+    fi
+    ifconfig wlan0 up > /dev/null
+    if [ $? -ne 0 ] ; then
+        echo "Failed to start TI wifi, interface can not be brought up."
+        return ${TI_WIFI_PA_NO_IF_UP_ERR}
+    fi
+
+    echo "TI Wifi module started"
+    return 0
+}
+
+ti_wifi_stop()
+{
+    echo "Stopping TI Wifi module"
+
+    ifconfig | grep wlan0 >/dev/null
+    if [ $? -eq 0 ]; then
+        ifconfig wlan0 down
+    fi
+    lsmod | grep wlcore >/dev/null
+
+    # If module unloading fails, it may be for different reasons: someone is
+    # using it, cannot be unloaded because it crashed, etc. Since there is
+    # nothing we can do about it, we should just continue on.
+    if [ $? -eq 0 ]; then
+        kmod unload wl18xx.ko >/dev/null 2>&1
+        kmod unload wlcore_sdio.ko >/dev/null 2>&1
+        kmod unload wlcore.ko >/dev/null 2>&1
+
+        #Do the necessary hardware settings for wlan
+        [ -z "$BOARD_CONFIG_WLAN" ] || ${BOARD_CONFIG_WLAN} REVERT
+    fi
+
+    echo "TI Wifi module stopped"
+    return 0
+}
+
+################################
 
 # Check the connection on the WiFi network interface.
 # Exit with 0 if connected otherwise exit with 8 (time out)
@@ -53,9 +322,9 @@ WiFiReset()
     for i in $(seq 1 ${retries})
     do
         sleep 1
-        /etc/init.d/tiwifi stop
+        ti_wifi_stop
         sleep 1
-        /etc/init.d/tiwifi start && exit 0
+        ti_wifi_start && exit 0
     done
     exit 127
 }
@@ -64,24 +333,24 @@ case ${CMD} in
   WIFI_START)
     echo "WIFI_START"
     # Mount the WiFi network interface
-    /etc/init.d/tiwifi start && exit 0
+    ti_wifi_start && exit 0
     # Store failure reason
     FAILUREREASON=$?
-    # If tiwifi.sh indicates firmware fails to boot, do reset
+    # If ti_wifi_start indicates firmware fails to boot, do reset
     if [ ${FAILUREREASON} -eq ${FIRMWAREFAILURE} ]; then
         WiFiReset && exit 0
         echo "Reset fail, perform clean up"
-        /etc/init.d/tiwifi stop
+        ti_wifi_stop
         exit ${FAILUREREASON}
     fi
     # Hardware is absent, do clean up
     if [ ${FAILUREREASON} -eq ${HARDWAREABSENCE} ]; then
         echo "Hardware is absent, perform clean up"
-        /etc/init.d/tiwifi stop
+        ti_wifi_stop
         exit ${FAILUREREASON}
     fi
     echo "Clean up due to unknown error"
-    /etc/init.d/tiwifi stop
+    ti_wifi_stop
     exit ${FAILUREREASON} ;;
 
   WIFI_STOP)
@@ -90,7 +359,7 @@ case ${CMD} in
     (/bin/ps -ax | grep wpa_supplicant | grep ${IFACE} >/dev/null 2>&1) \
     && /sbin/wpa_cli -i${IFACE} terminate
     # Unmount the WiFi network interface
-    /etc/init.d/tiwifi stop || exit 127
+    ti_wifi_stop || exit 127
     exit 0 ;;
 
   WIFI_SET_EVENT)
@@ -128,7 +397,7 @@ case ${CMD} in
     echo "WIFIAP_HOSTAPD_START"
     (/bin/hostapd /tmp/hostapd.conf -i${IFACE} -B) && exit 0
     # Fail to start hostapd, do cleanup
-    /etc/init.d/tiwifi stop
+    ti_wifi_stop
     exit 127 ;;
 
   WIFIAP_HOSTAPD_STOP)
